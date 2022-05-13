390ee4638b27987b6725a789cd695c3b90f768fc
LukasKastberger <lukas_kastberger@outlook.at>
2022-05-13 15:55:28 +0200

    added main activity and service

---
+++ app/src/main/java/com/android_motion_sensor_app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android_motion_sensor_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MqttService.class);
+        startService(intent);
+    }
+}
